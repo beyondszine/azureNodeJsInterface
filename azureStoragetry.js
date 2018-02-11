d503374749ee44e123a94ec48e540bc6df2f2f5a
saurabh shandilya <saurabhshandilya.1991@gmail.com>
2018-02-11 14:49:26 +0530

    working & exported upload & download functions

--- azureStoragetry.js
+++ azureStoragetry.js
@@ -1,41 +1,85 @@
 require('dotenv').config();
 var azure = require('azure-storage');
+var fs  = require('fs');
 
 if(process.env.AZURE_STORAGE_ACCOUNT && process.env.AZURE_STORAGE_ACCESS_KEY){
-  console.log(process.env.AZURE_STORAGE_ACCOUNT,process.env.AZURE_STORAGE_ACCESS_KEY);
+  //console.log(process.env.AZURE_STORAGE_ACCOUNT,process.env.AZURE_STORAGE_ACCESS_KEY);
 }
 else{
-  console.log('No sufficient environemtn variables found');
+  console.log('No sufficient environment variables found');
+  //process.exit(1);
 }
 var fileService = azure.createFileService(process.env.AZURE_STORAGE_ACCOUNT,process.env.AZURE_STORAGE_ACCESS_KEY);
 
-fileService.createShareIfNotExists('taskshare',function(error, result, response) {
+
+// export default function uploadFileAzure (mdirectory,mfilepath){
+var uploadFileAzure=function(mdirectory,mfilepath){
+  fileService.createShareIfNotExists(mdirectory,function(error, result, response) {
+    if (!error) {
+      //console.log('worked fine',result,response);
+      let uploadedFileName=mfilepath.split('/').pop();
+      console.log(uploadedFileName);
+      fileService.createFileFromLocalFile(mdirectory, '',uploadedFileName,mfilepath, function(error, result, response) {
+        if (error) {
+          // file uploaded
+          console.log('error occured while uploading',error);
+        }
+        else{
+          let newres=JSON.stringify(response);
+          //console.log(newres);
+          let newresjson=JSON.parse(newres);
+          if(newresjson['isSuccessful']==true && newresjson['statusCode']==200){
+            console.log('Uploaded successfully:',fileService.getUrl(mdirectory,'',uploadedFileName));
+          }
+          else{
+            console.log('failed somewhere');
+          }
+        }
+      });
+    }
+    else{
+      console.log(error);
+    }
+  })
+};
+
+
+var downloadFileAzure=function(mdirectory,mFileName){
+
+  fileService.getFileToStream(mdirectory, '', mFileName, fs.createWriteStream(mFileName), function(error, result, response) {
   if (!error) {
-    console.log('worked fine',result,response);
-    fileService.createFileFromLocalFile('taskshare', '','cam_config.png','cam_config.png', function(error, result, response) {
-      if (error) {
-        // file uploaded
-        console.log('error occured whiel uploading',error);
-      }
-      else{
-        console.log(result);
-        console.log(response);
-      }
-    });
+    //console.log(response);
+    let newres=JSON.stringify(response);
+    let newresjson=JSON.parse(newres);
+    if(newresjson['isSuccessful']==true && newresjson['statusCode']==200){
+      console.log('Downloaded successfully:');
+    }
+    else{
+      console.log('failed somewhere');
+    }
+
+    // file retrieved
   }
   else{
     console.log(error);
+    fs.unlink(mFileName,function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        //console.log('deleted successfully');
+      }
+    })
   }
 });
+};
 
 
-console.log(fileService.getUrl('taskshare','','cam_config.png'));
 
-// fileService.getShareAcl('taskshare',function(err,res){
-//   if(err){
-//     console.log('error occured',err);
-//   }
-//   else{
-//     console.log(res);
-//   }
-// });
+module.exports  = {
+  uploadFileAzure : uploadFileAzure,
+  downloadFileAzure : downloadFileAzure
+};
+
+uploadFileAzure('newdir','/home/beyond/nodejs/samples/awss3try/dashboard_nervecenter.png');
+downloadFileAzure('newdir','dashboard_nervecenter.png');
